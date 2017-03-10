9006fb4d2299bf052c258ee0f89769802ede5d45
Gunwoo Kim <gunwooterry@gmail.com>
2017-03-11 00:36:03 +0900

    Members are shown as text, not cards

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -2,56 +2,18 @@
 	<div id="members">
     <div class="ui text container">
       <h1></h1>
-      <h1 class="ui header">Staff</h1>
-    </div>
-    <div class="ui basic container segment">
-      <div class="ui grid">
-        <div class="eleven wide centered column">
-          <div class="ui four doubling cards">
-            <div class="card">
+      <h1 class="ui dividing header">Staff</h1>
+      <h4></h4>
+      <div class="ui four column grid">
+        <div class="column" v-for="n in 10">
+          <div class="ui items">
+            <div class="item content">
               <div class="content">
                 <div class="header">이주영</div>
                 <div class="meta">회장, KAIST 15</div>
-                <a>null@sparcs.org</a>
-              </div>
-            </div>
-            <div class="card">
-              <div class="content">
-                <div class="header">Jooyoung Lee</div>
-                <div class="meta">President, KAIST 15</div>
-                <a>null@sparcs.org</a>
-              </div>
-            </div>
-            <div class="card">
-              <div class="content">
-                <div class="header">Jooyoung Lee</div>
-                <div class="meta">President, KAIST 15</div>
-                <a>null@sparcs.org</a>
-              </div>
-            </div>
-            <div class="card">
-              <div class="content">
-                <div class="header">Jooyoung Lee</div>
-                <div class="meta">President, KAIST 15</div>
-                <a>null@sparcs.org</a>
-              </div>
-            </div>
-            <div class="card">
-              <div class="content">
-                <div class="header">Jooyoung Lee</div>
-                <div class="meta">President, KAIST 15</div>
-                <a>null@sparcs.org</a>
-              </div>
-            </div>
-            <div class="card">
-              <div class="content">
-                <div class="header">Jooyoung Lee</div>
-                <div class="meta">President, KAIST 15</div>
-                <a>null@sparcs.org</a>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
