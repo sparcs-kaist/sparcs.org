5f077e177348b871b52143c6e34778c12ea97147
Gunwoo Kim <gunwooterry@gmail.com>
2017-03-11 01:46:48 +0900

    Enable ESLint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,28 @@
+import $ from 'jquery'
 
-/* eslint-disable */
 export default {
   name: 'app',
-  components:{
+  components: {
   },
   data () {
     return {
 
     }
   },
-  methods:{
-    showLogin: function(){
+  methods: {
+    showLogin: function () {
 
     }
   },
-  mounted: function(){
-    $(document).ready(function(){
-      $('.ui.secondary.pointing.menu a.item').on('click', function(){
-        $(this).addClass('active').siblings().removeClass('active');
-      });
-      $('#sparcs_logo').on('click', function(){
-        $('.right.menu a.item').removeClass('active');
-      });
-      $('.ui.dropdown').dropdown();
-    });
-
+  mounted: function () {
+    $(document).ready(function () {
+      $('.ui.secondary.pointing.menu a.item').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active')
+      })
+      $('#sparcs_logo').on('click', function () {
+        $('.right.menu a.item').removeClass('active')
+      })
+      $('.ui.dropdown').dropdown()
+    })
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-/* eslint-disable */
 import Vue from 'vue'
 import App from './components/App'
 import router from './router'
 import '../node_modules/semantic-ui/dist/semantic.min.css'
-import semantic from 'semantic'
+// import semantic from 'semantic'
 
 Vue.config.productionTip = false
 
