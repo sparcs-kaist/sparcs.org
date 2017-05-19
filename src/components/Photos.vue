f362ad5bfc5933712c566a97c5a7db960bf1ed1b
Youngkyu <yk.hong@kaist.ac.kr>
2017-05-19 16:14:30 +0900

    Photo setting

--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,13 +1,39 @@
 <template>
 	<div id="photo">
-		Photo!
-		<div>
+
 		<button class="ui primary button photo" @click="addPhoto">
 			Add
 		</button>
 		<button class="ui button photo" @click="addPhoto2">
 			Add
 		</button>
+		<div class="ui vertical menu">
+			<div class="item">
+				<div class="ui input"><input type="text" placeholder="Search..."></div>
+			</div>
+			<div class="item">
+				Home
+				<div class="menu">
+					<a class="active item">Search</a>
+					<a class="item">Add</a>
+					<a class="item">Remove</a>
+				</div>
+			</div>
+			<a class="item">
+				<i class="grid layout icon"></i> Browse
+			</a>
+			<a class="item">
+				Messages
+			</a>
+			<div class="ui dropdown item">
+				More
+				<i class="dropdown icon"></i>
+				<div class="menu">
+					<a class="item"><i class="edit icon"></i> Edit Profile</a>
+					<a class="item"><i class="globe icon"></i> Choose Language</a>
+					<a class="item"><i class="settings icon"></i> Account Settings</a>
+				</div>
+			</div>
 		</div>
 		<div class="ui grid photo">
 			<div class="three column row" >
@@ -50,6 +76,7 @@
 	.ui.grid.photo{
 		padding-left : 100px;
 		padding-right: 100px;
+		float: left;
 	}
 	.ui.button.photo{
 		float: right;
@@ -58,5 +85,8 @@
 	.column.photo{
 		margin-bottom: 25px;
 	}
-
+	.ui.vertical.menu{
+		text-align: left;
+		float: left;
+	}
 </style>
