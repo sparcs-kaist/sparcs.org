acb0f9ce1dcd5d9c5c14fe59a8a1b4087cfbe6e8
Youngkyu <yk.hong@kaist.ac.kr>
2017-08-28 02:01:26 +0900

    DONE

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -75,7 +75,7 @@
         <span class="ara-brief">개설 과목을 한 눈에 보여주는 서비스</span>
         <h1 class="brand-name service-name">OTL PLUS</h1>
         <br>
-        <a class="service-link" href="http://otlplus.kaist.ac.kr"><span class="ara-link"><u>GO TO OTL PLUS PAGE</u></span></a>
+        <a class="service-link" href="http://otlplus.sparcs.org"><span class="ara-link"><u>GO TO OTL PLUS PAGE</u></span></a>
         <br>
         <br>
         <p class="service-description"> &nbsp;&nbsp;&nbsp;&nbsp;OTL PLUS는 매 학기 개설되는 KAIST의 대부분의 과목들에 대한 정보를 쉽게 검색할 수 있게 도와줍니다. 과목들을 개인 시간표에 추가할 수 있으며 여러 개의 시간표를 한 번에 관리할 수 있습니다. 뿐만 아니라, 해당 과목에 대한 실라버스 및 해당 과목을 수강한 여러 학우들의 수강 후기를 볼 수 있어 수강 신청에 있어 많은 편의를 제공합니다. 시험을 망칠 게 뻔하니 미리 와서 다음 학기를 준비하세요.</p>
