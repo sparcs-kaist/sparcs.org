4f4e444012c206c061441ea8770b9874eada68f9
Gunwoo Kim <gunwooterry@gmail.com>
2018-01-05 00:41:52 +0900

    fix(projects): separate projects data into json

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -8,152 +8,58 @@
 		</div>
 		<div class="ui container">
 			<div class="ui stackable center aligned three column grid">
-        <div class="column">
-          <div style="width:80%; margin:10%;">
-            <img class="ui small centered image" src="./../../static/project-icon.png"/>
+        <div
+          v-for="project in projects"
+          class="column"
+        >
+          <div style="width: 80%; margin: 10%;">
+            <img
+              class="ui small centered image"
+              src="./../../static/project-icon.png"
+            />
             <h2 class="ui header">
-              SPARCS SSO
-              <div class="sub header">PM : hou</div>
+              {{project.title}}
             </h2>
+            <div class="sub header">PM : {{project.pm}}</div>
             <br>
-            SPARCS 서비스 로그인을 하나의 계정으로 할 수 있도록 해주는 Single Sign On 서비스입니다. KAIST Portal 연동을 통해 KAIST 구성원임을 인증하고 실명, 생일, 학번 등 개인 정보를 저장하여 SPARCS 서비스에 제공합니다.
+            {{project.description}}
             <br><br>
-            <a href="https://sparcssso.kaist.ac.kr"><div style="display:inline;"><i class="big home icon link icon" style="color:black;"></i></div></a>
-            <a href="https://github.com/sparcs-kaist/sparcssso"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+            <a
+              v-if="project.homepage"
+              href="{{project.homepage}}"
+            >
+              <div style="display: inline;">
+                <i class="big home icon link icon" style="color: black;"></i>
+              </div>
+            </a>
+            <a
+              v-if="project.github"
+              href="{{project.github}}"
+            >
+              <div style="display: inline;">
+                <i class="big github alternate icon link icon" style="color: black;"></i>
+              </div>
+            </a>
           </div>
         </div>
-        <div class="column">
-          <div style="width:80%; margin:10%;">
-            <img class="ui small centered image" src="./../../static/project-icon.png"/>
-            <h2 class="ui header">
-              SPARCS 09
-              <div class="sub header">PM : sugoi</div>
-            </h2>
-            <br>
-            SPARCS 내부의 공동구매 플랫폼으로 관리자는 공동 구매 아이템을 업로드하고 사용자는 옵션과 수량을 입력하여 공동 구매에 참여할 수 있습니다.
-            <br><br>
-            <a href="https://09.sparcs.org"><div style="display:inline;"><i class="big home icon link icon" style="color:black;"></i></div></a>
-            <a href="https://github.com/sparcs-kaist/sparcs09"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
-          </div>
-        </div>
-        <div class="column">
-          <div style="width:80%; margin:10%;">
-            <img class="ui small centered image" src="./../../static/project-icon.png"/>
-            <h2 class="ui header">
-              Nugu
-              <div class="sub header">PM : luan</div>
-            </h2>
-            <br>
-            SPARCS 회원의 정보를 보관하고 열람할 수 있는 서비스입니다.
-            <br><br>
-            <a href="https://github.com/sparcs-kaist/nugu"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
-            <a href="https://github.com/sparcs-kaist/nugu-api"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
-          </div>
-        </div>
-				<div class="column">
-					<div style="width:80%; margin:10%;">
-						<img class="ui small centered image" src="./../../static/project-icon.png"/>
-						<h2 class="ui header">
-							GenChem
-							<div class="sub header">PM : null</div>
-						</h2>
-						<br>
-						 화학과 과목들의 정보를 열람할 수 있는 gencheminkaist 사이트를 리뉴얼하는 프로젝트입니다. 기존 사이트의 사용성과 심미성을 향상시키는데 목표를 두고 있습니다.
-						<br><br>
-					</div>
-				</div>
-				<div class="column">
-					<div style="width:80%; margin:10%;">
-						<img class="ui small centered image" src="./../../static/project-icon.png"/>
-						<h2 class="ui header">
-							넙죽이
-							<div class="sub header">PM : pablo</div>
-						</h2>
-						<br>
-            학사일정 등 카이스트와 관련된 정보가 궁금할 때 어디서 확인해야할지 모르거나 포탈 등에 검색해도 원하는 결과를 얻지 못하는 경우가 많습니다. 넙죽이는 이들을 통합적으로 검색할 수 있는 서비스로, 챗봇 형태로 제공됩니다.
-            <br><br>
-					</div>
-				</div>
-        <div class="column">
-          <div style="width:80%; margin:10%;">
-            <img class="ui small centered image" src="./../../static/project-icon.png"/>
-            <h2 class="ui header">
-              New Ara
-              <div class="sub header">PM : raon</div>
-            </h2>
-            <br>
-            현재 운영 중인 학내 커뮤니티 Ara의 새로운 버전입니다. 기존의 Ara가 개발된 지 오랜 시간이 지나 레거시 코드가 많고, 업데이트가 힘들어져 새롭게 처음부터 개발할 예정입니다.
-            <br><br>
-          </div>
-        </div>
-				<div class="column">
-					<div style="width:80%; margin:10%;">
-						<img class="ui small centered image" src="./../../static/project-icon.png"/>
-						<h2 class="ui header">
-							Zabo
-							<div class="sub header">PM : ohimzsh</div>
-						</h2>
-						<br>
-						동아리를 비롯한 각종 단체들의 리크루팅, 공연 포스터를 온라인으로 게시할 수 있는 서비스입니다. 학우들은 관심있는 단체의 행사를 보다 쉽게 검색할 수 있고, 단체들은 경쟁없이 편하게 홍보를 할 수 있습니다
-						<br><br>
-					</div>
-				</div>
-				<div class="column">
-					<div style="width:80%; margin:10%;">
-						<img class="ui small centered image" src="./../../static/project-icon.png"/>
-						<h2 class="ui header">
-							OTL PLUS
-							<div class="sub header">PM : zealot</div>
-						</h2>
-						<br>
-						카이스트 학우들의 뜨거운 관심을 받고 있는 과목후기/모의시간표 서비스 OTL을 개선시킨 새로운 서비스입니다. 현재 과목 후기 사이트가 OTL+로 대체되었으며 곧 시간표에도 OTL+가 적용될 예정입니다
-						<br><br>
-					</div>
-				</div>
-				<div class="column">
-					<div style="width:80%; margin:10%;">
-						<img class="ui small centered image" src="./../../static/project-icon.png"/>
-						<h2 class="ui header">
-							ARA
-							<div class="sub header">PM : jara</div>
-						</h2>
-						<br>
-						KAIST 구성원들의 커뮤니티입니다. 학내 중요공지 뿐만 아니라 구인/구직, 중고거래 글 등 많은 종류의 글들이 게시됩니다. KAIST 구성원이라면 누구나 사용할 수 있습니다.
-						<br><br>
-					</div>
-				</div>
-				<div class="column">
-					<div style="width:80%; margin:10%;">
-						<img class="ui small centered image" src="./../../static/project-icon.png"/>
-						<h2 class="ui header">
-							거울
-							<div class="sub header">PM : jambo</div>
-						</h2>
-						<br>
-            전세계적으로 자주 사용하는 자유/공개 소프트웨어를 미러링하는 서비스입니다. 거울을 통해 아시아 지역의 컴퓨터 사용자들은 오픈 소스 소프트웨어를 가까운 SPARCS 컴퓨터를 통해 다운받을 수 있습니다.
-						<br><br>
-					</div>
-				</div>
-				<div class="column">
-					<div style="width:80%; margin:10%;">
-						<img class="ui small centered image" src="./../../static/project-icon.png"/>
-						<h2 class="ui header">
-							Heartbeat
-							<div class="sub header">PM : jambo, sugoi</div>
-						</h2>
-						<br>
-            Heartbeat는 SPARCS 물리서버와 가상서버의 상태를 지속적으로 Web을 통해 편하게 모니터링할 수 있게 해주는 서비스입니다.
-						<br><br>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import projects from '../text/projects';
+
 export default {
   name: 'Projects',
+  data: () => ({
+    projects: [],
+  }),
+
+  created() {
+    this.projects = JSON.parse(projects);
+  },
+
   mounted() {
     document.getElementById('projects').classList.add('active');
     // we gave marginTop of r_view when righ menu item is clicked, so reload doesn't take this action
