de7990f73726f2b2a85547ca39af476aa429d0ac
yongsu-bloo <dydtnrkwkd@kaist.ac.kr>
2018-11-05 03:28:08 +0900

    Project information update

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,3 +1,4 @@
+
 <template>
 	<div>
 		<div id="projectHeader" class="ui vertical masthead center aligned basic segment">
@@ -8,12 +9,13 @@
 		</div>
 		<div class="ui container">
 			<div class="ui stackable center aligned three column grid">
+
         <div class="column">
           <div style="width:80%; margin:10%;">
             <img class="ui small centered image" src="./../../static/project-icon.png"/>
             <h2 class="ui header">
               SPARCS SSO
-              <div class="sub header">PM : hou</div>
+              <div class="sub header">PM : kyeome</div>
             </h2>
             <br>
             SPARCS 서비스 로그인을 하나의 계정으로 할 수 있도록 해주는 Single Sign On 서비스입니다. KAIST Portal 연동을 통해 KAIST 구성원임을 인증하고 실명, 생일, 학번 등 개인 정보를 저장하여 SPARCS 서비스에 제공합니다.
@@ -22,82 +24,20 @@
             <a href="https://github.com/sparcs-kaist/sparcssso"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
           </div>
         </div>
-        <div class="column">
-          <div style="width:80%; margin:10%;">
-            <img class="ui small centered image" src="./../../static/project-icon.png"/>
-            <h2 class="ui header">
-              SPARCS 09
-              <div class="sub header">PM : sugoi</div>
-            </h2>
-            <br>
-            SPARCS 내부의 공동구매 플랫폼으로 관리자는 공동 구매 아이템을 업로드하고 사용자는 옵션과 수량을 입력하여 공동 구매에 참여할 수 있습니다.
-            <br><br>
-            <a href="https://09.sparcs.org"><div style="display:inline;"><i class="big home icon link icon" style="color:black;"></i></div></a>
-            <a href="https://github.com/sparcs-kaist/sparcs09"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
-          </div>
-        </div>
-        <div class="column">
-          <div style="width:80%; margin:10%;">
-            <img class="ui small centered image" src="./../../static/project-icon.png"/>
-            <h2 class="ui header">
-              Nugu
-              <div class="sub header">PM : luan</div>
-            </h2>
-            <br>
-            SPARCS 회원의 정보를 보관하고 열람할 수 있는 서비스입니다.
-            <br><br>
-            <a href="https://github.com/sparcs-kaist/nugu"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
-            <a href="https://github.com/sparcs-kaist/nugu-api"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
-          </div>
-        </div>
+
 				<div class="column">
 					<div style="width:80%; margin:10%;">
 						<img class="ui small centered image" src="./../../static/project-icon.png"/>
 						<h2 class="ui header">
-							GenChem
-							<div class="sub header">PM : null</div>
+							Guideline
+							<div class="sub header">PM : leo</div>
 						</h2>
 						<br>
-						 화학과 과목들의 정보를 열람할 수 있는 gencheminkaist 사이트를 리뉴얼하는 프로젝트입니다. 기존 사이트의 사용성과 심미성을 향상시키는데 목표를 두고 있습니다.
+						SPARCS 서비스를 위한 디자인 가이드라인을 설립하고 고유한 이미지를 구축하는 프로젝트입니다. 가이드라인 문서를 바탕으로, 새로 출시되는 서비스는 물론 기존의 서비스도 모두 새롭게 디자인됩니다. 디자인 가이드라인은 디자인 팀의 지속적인 피드백과 업데이트를 통해 발전해 나가고 있습니다.
 						<br><br>
 					</div>
 				</div>
-				<div class="column">
-					<div style="width:80%; margin:10%;">
-						<img class="ui small centered image" src="./../../static/project-icon.png"/>
-						<h2 class="ui header">
-							넙죽이
-							<div class="sub header">PM : pablo</div>
-						</h2>
-						<br>
-            학사일정 등 카이스트와 관련된 정보가 궁금할 때 어디서 확인해야할지 모르거나 포탈 등에 검색해도 원하는 결과를 얻지 못하는 경우가 많습니다. 넙죽이는 이들을 통합적으로 검색할 수 있는 서비스로, 챗봇 형태로 제공됩니다.
-            <br><br>
-					</div>
-				</div>
-        <div class="column">
-          <div style="width:80%; margin:10%;">
-            <img class="ui small centered image" src="./../../static/project-icon.png"/>
-            <h2 class="ui header">
-              New Ara
-              <div class="sub header">PM : raon</div>
-            </h2>
-            <br>
-            현재 운영 중인 학내 커뮤니티 Ara의 새로운 버전입니다. 기존의 Ara가 개발된 지 오랜 시간이 지나 레거시 코드가 많고, 업데이트가 힘들어져 새롭게 처음부터 개발할 예정입니다.
-            <br><br>
-          </div>
-        </div>
-				<div class="column">
-					<div style="width:80%; margin:10%;">
-						<img class="ui small centered image" src="./../../static/project-icon.png"/>
-						<h2 class="ui header">
-							Zabo
-							<div class="sub header">PM : ohimzsh</div>
-						</h2>
-						<br>
-						동아리를 비롯한 각종 단체들의 리크루팅, 공연 포스터를 온라인으로 게시할 수 있는 서비스입니다. 학우들은 관심있는 단체의 행사를 보다 쉽게 검색할 수 있고, 단체들은 경쟁없이 편하게 홍보를 할 수 있습니다
-						<br><br>
-					</div>
-				</div>
+				
 				<div class="column">
 					<div style="width:80%; margin:10%;">
 						<img class="ui small centered image" src="./../../static/project-icon.png"/>
@@ -106,40 +46,206 @@
 							<div class="sub header">PM : zealot</div>
 						</h2>
 						<br>
-						카이스트 학우들의 뜨거운 관심을 받고 있는 과목후기/모의시간표 서비스 OTL을 개선시킨 새로운 서비스입니다. 현재 과목 후기 사이트가 OTL+로 대체되었으며 곧 시간표에도 OTL+가 적용될 예정입니다
+						카이스트 학우들의 뜨거운 관심을 받고 있는 서비스 OTL을 개선하는 프로젝트입니다. 기존 과목후기·모의시간표 기능을 개편하며 새로운 기능을 추가합니다.
 						<br><br>
+						<a href="https://otl.kaist.ac.kr/"><div style="display:inline;"><i class="big home icon link icon" style="color:black;"></i></div></a>
+						<a href="https://github.com/sparcs-kaist/otlplus"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
 					</div>
 				</div>
+
 				<div class="column">
 					<div style="width:80%; margin:10%;">
 						<img class="ui small centered image" src="./../../static/project-icon.png"/>
 						<h2 class="ui header">
 							ARA
-							<div class="sub header">PM : jara</div>
+							<div class="sub header">PM : jaja</div>
 						</h2>
 						<br>
 						KAIST 구성원들의 커뮤니티입니다. 학내 중요공지 뿐만 아니라 구인/구직, 중고거래 글 등 많은 종류의 글들이 게시됩니다. KAIST 구성원이라면 누구나 사용할 수 있습니다.
 						<br><br>
+						<a href="https://ara.kaist.ac.kr/"><div style="display:inline;"><i class="big home icon link icon" style="color:black;"></i></div></a>
 					</div>
 				</div>
+
+				<div class="column">
+					<div style="width:80%; margin:10%;">
+						<img class="ui small centered image" src="./../../static/project-icon.png"/>
+						<h2 class="ui header">
+							New Ara
+							<div class="sub header">PM : yujingaya</div>
+						</h2>
+						<br>
+						현재 운영 중인 학내 커뮤니티 Ara의 새로운 버전입니다. 기존의 Ara가 개발된 지 오랜 시간이 지나 레거시 코드가 많고, 업데이트가 힘들어져 새롭게 처음부터 개발중입니다.
+						<br><br>
+						<a href="https://github.com/sparcs-kaist/new-ara-web"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+						<a href="https://github.com/sparcs-kaist/new-ara-api"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+					</div>
+				</div>
+
+				<div class="column">
+					<div style="width:80%; margin:10%;">
+						<img class="ui small centered image" src="./../../static/project-icon.png"/>
+						<h2 class="ui header">
+							AraGame
+							<div class="sub header">PM : stark</div>
+						</h2>
+						<br>
+						리뉴얼이 진행 중인 학내 커뮤니티 New ARA에 업로드 될 간단하고 익숙한 웹 게임을 만드는 프로젝트입니다. 테트리스를 기반으로 한 변형 게임을 제작중입니다.
+						<br><br>
+						<a href="https://github.com/sparcs-kaist/aragame"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+					</div>
+				</div>
+
+				<div class="column">
+					<div style="width:80%; margin:10%;">
+						<img class="ui small centered image" src="./../../static/project-icon.png"/>
+						<h2 class="ui header">
+							Zabo
+							<div class="sub header">PM : jara</div>
+						</h2>
+						<br>
+						동아리를 비롯한 각종 단체들의 리크루팅, 공연 포스터를 온라인으로 게시할 수 있는 서비스입니다. 학우들은 관심있는 단체의 행사를 보다 쉽게 검색할 수 있고, 단체들은 경쟁없이 편하게 홍보를 할 수 있습니다.
+						<br><br>
+						<a href="http://zabo.sparcs.org"><div style="display:inline;"><i class="big home icon link icon" style="color:black;"></i></div></a>
+						<a href="https://github.com/sparcs-kaist/zabo-web"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+						<a href="https://github.com/sparcs-kaist/zabo-api"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+					</div>
+				</div>
+
+				<div class="column">
+					<div style="width:80%; margin:10%;">
+						<img class="ui small centered image" src="./../../static/project-icon.png"/>
+						<h2 class="ui header">
+							Paper
+							<div class="sub header">PM : jara</div>
+						</h2>
+						<br>
+						동아리를 비롯한 각종 단체들의 리쿠르팅 밎 행사 참여를 도와주는 서비스입니다. 참가자들은 paper를 통해 참가를 할 수 있습니다. 개설자는 paper를 통해서 참가한 학생들의 관리를 편하게 해줍니다.
+						<br><br>
+						<a href="https://github.com/sparcs-kaist/paper-web"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+						<a href="https://github.com/sparcs-kaist/paper-api"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+					</div>
+				</div>
+
+				<div class="column">
+					<div style="width:80%; margin:10%;">
+						<img class="ui small centered image" src="./../../static/project-icon.png"/>
+						<h2 class="ui header">
+							Wika
+							<div class="sub header">PM : appleseed</div>
+						</h2>
+						<br>
+						위카 Wika는 KAIST 내 택시 카풀 지원 서비스입니다. 택시를 탈 예정인 시간대, 합승할 인원, 목적지 등을 사용자로부터 입력받아 파티를 매칭해줍니다. 학생들은 택시비 부담을 덜 수 있고, 택시 승강장의 혼잡도 개선에도 도움을 줄 수 있습니다.
+						<br><br>
+						<a href="https://github.com/sparcs-kaist/wika"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+					</div>
+				</div>
+
+				<div class="column">
+					<div style="width:80%; margin:10%;">
+						<img class="ui small centered image" src="./../../static/project-icon.png"/>
+						<h2 class="ui header">
+							Wheel
+							<div class="sub header">PM : medowhill</div>
+						</h2>
+						<br>
+						Wheel은 SPARCS의 모든 서버의 root 권한을 가지고 관리하는 그룹입니다. 서버의 상태를 관리하여 SPARCS의 서비스들이 원할하게 학생들에게 제공될 수 있도록 합니다.
+						<br><br>
+					</div>
+				</div>
+
+				<div class="column">
+					<div style="width:80%; margin:10%;">
+						<img class="ui small centered image" src="./../../static/project-icon.png"/>
+						<h2 class="ui header">
+							Home
+							<div class="sub header">PM : bloo</div>
+						</h2>
+						<br>
+						SPARCS 공식 홈페이지는 SPARCS가 학우들과 소통할 수 있는 창구로서 현재 운영, 개발중인 SPARCS의 다양한 서비스들을 소개하고 SPARCS의 활동 및 현황을 공유하는 공간입니다.
+						<br><br>
+						<a href="https://sparcs.org/"><div style="display:inline;"><i class="big home icon link icon" style="color:black;"></i></div></a>
+						<a href="https://github.com/sparcs-kaist/sparcs.org"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+					</div>
+				</div>
+
+        <div class="column">
+          <div style="width:80%; margin:10%;">
+            <img class="ui small centered image" src="./../../static/project-icon.png"/>
+            <h2 class="ui header">
+              SPARCS 09
+              <!-- <div class="sub header">PM : sugoi</div> -->
+            </h2>
+            <br>
+            SPARCS 내부의 공동구매 플랫폼으로 관리자는 공동 구매 아이템을 업로드하고 사용자는 옵션과 수량을 입력하여 공동 구매에 참여할 수 있습니다.
+            <br><br>
+						<a href="https://09.sparcs.org"><div style="display:inline;"><i class="big home icon link icon" style="color:black;"></i></div></a>
+            <a href="https://github.com/sparcs-kaist/sparcs09"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+          </div>
+        </div>
+
+        <div class="column">
+          <div style="width:80%; margin:10%;">
+            <img class="ui small centered image" src="./../../static/project-icon.png"/>
+            <h2 class="ui header">
+              Nugu
+              <!-- <div class="sub header">PM : luan</div> -->
+            </h2>
+            <br>
+            SPARCS 회원의 정보를 보관하고 열람할 수 있는 서비스입니다.
+            <br><br>
+            <a href="https://github.com/sparcs-kaist/nugu"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+            <a href="https://github.com/sparcs-kaist/nugu-api"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+          </div>
+        </div>
+
+				<div class="column">
+					<div style="width:80%; margin:10%;">
+						<img class="ui small centered image" src="./../../static/project-icon.png"/>
+						<h2 class="ui header">
+							GenChem
+							<!-- <div class="sub header">PM : null</div> -->
+						</h2>
+						<br>
+						 화학과 과목들의 정보를 열람할 수 있는 gencheminkaist 사이트를 리뉴얼하는 프로젝트입니다. 기존 사이트의 사용성과 심미성을 향상시키는데 목표를 두고 있습니다.
+						<br><br>
+						<a href="https://github.com/sparcs-kaist/gen_chem"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+					</div>
+				</div>
+
+				<div class="column">
+					<div style="width:80%; margin:10%;">
+						<img class="ui small centered image" src="./../../static/project-icon.png"/>
+						<h2 class="ui header">
+							넙죽이
+							<!-- <div class="sub header">PM : pablo</div> -->
+						</h2>
+						<br>
+            학사일정 등 카이스트와 관련된 정보가 궁금할 때 어디서 확인해야할지 모르거나 포탈 등에 검색해도 원하는 결과를 얻지 못하는 경우가 많습니다. 넙죽이는 이들을 통합적으로 검색할 수 있는 서비스로, 챗봇 형태로 제공됩니다.
+            <br><br>
+						<a href="https://github.com/sparcs-kaist/Neobjugi"><div style="display:inline;"><i class="big github alternate icon link icon" style="color:black;"></i></div></a>
+					</div>
+				</div>
+
 				<div class="column">
 					<div style="width:80%; margin:10%;">
 						<img class="ui small centered image" src="./../../static/project-icon.png"/>
 						<h2 class="ui header">
 							거울
-							<div class="sub header">PM : jambo</div>
+							<!-- <div class="sub header">PM : jambo</div> -->
 						</h2>
 						<br>
             전세계적으로 자주 사용하는 자유/공개 소프트웨어를 미러링하는 서비스입니다. 거울을 통해 아시아 지역의 컴퓨터 사용자들은 오픈 소스 소프트웨어를 가까운 SPARCS 컴퓨터를 통해 다운받을 수 있습니다.
 						<br><br>
 					</div>
 				</div>
+
 				<div class="column">
 					<div style="width:80%; margin:10%;">
 						<img class="ui small centered image" src="./../../static/project-icon.png"/>
 						<h2 class="ui header">
 							Heartbeat
-							<div class="sub header">PM : jambo, sugoi</div>
+							<!-- <div class="sub header">PM : jambo, sugoi</div> -->
 						</h2>
 						<br>
             Heartbeat는 SPARCS 물리서버와 가상서버의 상태를 지속적으로 Web을 통해 편하게 모니터링할 수 있게 해주는 서비스입니다.
